09fa89e27d7f706135f2ff3fa2464080c48df253
LehaShark <alexey99979@yandex.ru>
2021-10-04 22:14:36 +0300

    add config and restructure main

---
+++ config/config.py
@@ -0,0 +1,3 @@
+bootstrap_server1 = ["rc1a-k9hradi0d5djbco0.mdb.yandexcloud.net:9091"]
+bootstrap_server2 = ["rc1b-pq117mh7j9hmem2n.mdb.yandexcloud.net:9091"]
+bootstrap_server3 = ["rc1c-894cbo6uhb90srcg.mdb.yandexcloud.net:9091"]
--- main.py
+++ main.py
@@ -1,27 +1,28 @@
+from config import config
+from kafka import KafkaConsumer
+from json import load
+import json
+
+from config.config import bootstrap_server1, bootstrap_server2, bootstrap_server3
 
 if __name__ == '__main__':
-    from kafka import KafkaConsumer
-    from json import load
-    import json
-
     topic = 'trade'
     sasl_mechanism = "PLAIN"
     username = "forecast"
     password = "o$aPN26utpR@8ndcWU72"
     security_protocol = "SASL_SSL"
 
-    consumer = KafkaConsumer('trade',
-                             bootstrap_servers=['rc1c-894cbo6uhb90srcg.mdb.yandexcloud.net:9091'],
+    consumer = KafkaConsumer(topic,
+                             bootstrap_servers=bootstrap_server3,
                              api_version=(0, 10),
                              # security_protocol = security_protocol,
-                             # ssl_check_hostname = True,
+                             ssl_check_hostname = True,
                              sasl_mechanism=sasl_mechanism,
                              sasl_plain_username=username,
                              sasl_plain_password=password
                              )
     print('ok')
+    print(type(consumer))
     for message in consumer:
         print(message.value)
         print('ok')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
